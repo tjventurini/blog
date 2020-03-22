19fc055e775817713389b4b18c19ef9ccc0e27d9
Thomas Venturini <thomas@venturini.codes>
2020-03-22 05:16:12 +0100

    add directory view

---
+++ .vuepress/theme/layouts/Directory.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="page mx-auto max-w-screen-lg text-gray-700">
+        <Header/>
+        <div class="grid grid-cols-12">
+            <div class="col-span-8">
+                <PageTitle/>
+                <ArticleList :articles="articles"/>
+            </div>
+            <div class="col-span-4">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from '@theme/components/Header'
+    import ArticleList from '@theme/components/ArticleList'
+    import PageTitle from "../components/PageTitle";
+
+    export default {
+        components: {PageTitle, Header, ArticleList},
+        computed: {
+            articles() {
+                console.log(this.$site.pages);
+                return this.$site.pages.filter(function (page) {
+                    return /^\/articles\//.test(page.regularPath)
+                })
+                    .filter((page) => (page.title))
+                    .reverse()
+                    .slice(0, 5);
+            }
+        }
+    }
+</script>
