fffecdb7d07bf60286fdd1af8431c84556af4195
Thomas Venturini <thomas@venturini.codes>
2020-04-05 18:46:09 +0200

    small bugfix for the pagination on devices below sm

--- .vuepress/theme/components/Pagination.vue
+++ .vuepress/theme/components/Pagination.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="pagination grid grid-cols-3 gap-4">
+    <div class="pagination grid grid-cols-5 sm:grid-cols-3 gap-4 my-4">
         <div class="prev text-right">
             <a v-if="pagination.hasPrev" :href="pagination.prevLink"
                class="moving-gradient-bg text-white text-center px-4 py-2 rounded-lg">⬅</a>
         </div>
-        <div class="text-center">
+        <div class="text-center col-span-3 sm:col-span-1">
             <span class="moving-gradient-bg text-white text-center px-4 py-2 rounded-lg font-bold">Page {{
                 pagination.paginationIndex + 1 }} of {{ pagination.length }}</span>
         </div>
