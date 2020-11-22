4964bd9962eb14ae0499add24ea5ba9153f822f3
Thomas Venturini <thomas@venturini.codes>
2020-11-22 05:37:25 +0100

    only show published suggestions

--- .vuepress/theme/components/SearchBox.vue
+++ .vuepress/theme/components/SearchBox.vue
@@ -1,44 +1,41 @@
 <template>
     <div class="search-box">
         <input
-                ref="input"
-                aria-label="Search"
-                :value="query"
-                class="w-full"
-                :class="{ 'focused': focused }"
-                :placeholder="placeholder"
-                autocomplete="off"
-                spellcheck="false"
-                @input="query = $event.target.value"
-                @focus="focused = true"
-                @blur="focused = false"
-                @keyup.enter="go(focusIndex)"
-                @keyup.up="onUp"
-                @keyup.down="onDown"
-        >
+            ref="input"
+            aria-label="Search"
+            :value="query"
+            class="w-full"
+            :class="{ focused: focused }"
+            :placeholder="placeholder"
+            autocomplete="off"
+            spellcheck="false"
+            @input="query = $event.target.value"
+            @focus="focused = true"
+            @blur="focused = false"
+            @keyup.enter="go(focusIndex)"
+            @keyup.up="onUp"
+            @keyup.down="onDown"
+        />
         <ul
-                v-if="showSuggestions"
-                class="suggestions top-8"
-                :class="{ 'align-right': alignRight }"
-                @mouseleave="unfocus"
+            v-if="showSuggestions"
+            class="suggestions top-8"
+            :class="{ 'align-right': alignRight }"
+            @mouseleave="unfocus"
         >
             <li
-                    v-for="(s, i) in suggestions"
-                    :key="i"
-                    class="suggestion"
-                    :class="{ focused: i === focusIndex }"
-                    @mousedown="closeNavigationAndGo(i)"
-                    @mouseenter="focus(i)"
+                v-for="(s, i) in suggestions"
+                :key="i"
+                v-if="s.frontmatter.publish"
+                class="suggestion"
+                :class="{ focused: i === focusIndex }"
+                @mousedown="closeNavigationAndGo(i)"
+                @mouseenter="focus(i)"
             >
-                <a
-                        :href="s.path"
-                        @click.prevent
-                >
+                <a :href="s.path" @click.prevent>
                     <span class="page-title">{{ s.title || s.path }}</span>
-                    <span
-                            v-if="s.header"
-                            class="header"
-                    >&gt; {{ s.header.title }}</span>
+                    <span v-if="s.header" class="header"
+                        >&gt; {{ s.header.title }}</span
+                    >
                 </a>
             </li>
         </ul>
@@ -46,34 +43,36 @@
 </template>
 
 <script>
-    import SearchBox from '@SearchBox'
+import SearchBox from "@SearchBox";
 
-    export default {
-        name: "SearchBox",
-        extends: SearchBox,
-        methods: {
-            closeNavigationAndGo(i) {
-                document.querySelector('.close-navigation-overlay').dispatchEvent(new Event('click'))
-                this.go(i)
-            }
-        }
-    }
+export default {
+    name: "SearchBox",
+    extends: SearchBox,
+    methods: {
+        closeNavigationAndGo(i) {
+            document
+                .querySelector(".close-navigation-overlay")
+                .dispatchEvent(new Event("click"));
+            this.go(i);
+        },
+    },
+};
 </script>
 
 <style lang="sass">
-    .search-box
-        margin: 0
-        padding: 0
+.search-box
+    margin: 0
+    padding: 0
+    width: 100%
+    height: 100%
+
+    input
+        border-radius: 0.5rem
         width: 100%
         height: 100%
-
-        input
-            border-radius: 0.5rem
-            width: 100%
-            height: 100%
-            background-position: 0.5rem center
-            left: 0
-            @apply bg-gray-200
+        background-position: 0.5rem center
+        left: 0
+        @apply bg-gray-200
 
             &:focus, .focus
                 border-radius: 0.5rem
