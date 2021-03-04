e2bd5d594296e0a69e7cff160753a1c5c392d27a
Thomas Venturini <thomas@venturini.codes>
2021-03-04 21:09:04 +0100

    update footer

--- .vuepress/theme/components/Footer.vue
+++ .vuepress/theme/components/Footer.vue
@@ -2,8 +2,11 @@
     <div class="footer max-w-screen-lg mx-auto text-gray-700 text-center py-5">
         <div class="grid grid-cols-12">
             <div class="col-span-12 lg:col-span-8">
-                &copy; Thomas Venturini | Made with ♥ by
-                <a href="https://venturini.codes" target="_blank"
+                &copy; Thomas Venturini |
+                <a
+                    href="https://venturini.codes"
+                    target="_blank"
+                    title="Webdesign and Webdevelopment"
                     >venturini.codes</a
                 >
             </div>
