e6682f88994f34328efc2e04160da464b6e8f654
Thomas Venturini <thomas@venturini.codes>
2021-05-02 02:01:50 +0200

    fix grid in pagetitle component

--- .vuepress/theme/components/PageTitle.vue
+++ .vuepress/theme/components/PageTitle.vue
@@ -5,19 +5,25 @@
                 <i class="fa fa-hashtag moving-gradient-text align-middle"></i>
             </div>
         </div>
-        <div class="col-span-5 sm:col-span-7">
-            <h2><span class="align-middle heading-10 font-bold text-4xl">{{ pageTitle }}</span></h2>
+        <div class="col-span-5 sm:col-span-6 lg:col-span-7">
+            <h2>
+                <span class="align-middle heading-10 font-bold text-4xl">{{
+                    pageTitle
+                }}</span>
+            </h2>
         </div>
     </header>
 </template>
 <script>
-    export default {
-        name: 'PageTitle',
-        props: ['title'],
-        computed: {
-            pageTitle: function () {
-                return this.title || this.$page.title || this.$page.frontmatter.title
-            }
-        }
-    }
+export default {
+    name: "PageTitle",
+    props: ["title"],
+    computed: {
+        pageTitle: function () {
+            return (
+                this.title || this.$page.title || this.$page.frontmatter.title
+            );
+        },
+    },
+};
 </script>
